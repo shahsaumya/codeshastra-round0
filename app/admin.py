049c52d58fb808ca6210014d22de70bcd6bf37b7
Shreya Naik <naik.shreya2708@gmail.com>
2018-01-28 10:34:06 +0530

    Models added for allotment

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from app.models import Student_Detail, Teacher
+from app.models import Student_Detail, Teacher, Student_Allotment, Teacher_Allotment, Room
 
 admin.site.register(Student_Detail)
 admin.site.register(Teacher)
+admin.site.register(Student_Allotment)
+admin.site.register(Teacher_Allotment)
+admin.site.register(Room)
 # Register your models here.
